e61ba290f84853b04ffd4c53e1c59ea545a5ce56
Igor Clemente <igorelectrofest2011@hotmail.com>
2019-10-29 14:17:29 -0300

    BOOTCAMP_GOSTACK_MODULE10_REDUXSAGAUPDATEPROFILE_CREATED_29102019

--- module10/src/pages/Profile/index.js
+++ module10/src/pages/Profile/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
 import { Container } from './styles';
 
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
 export default function Profile() {
   const profile = useSelector(state => state.user.profile);
+  const dispatch = useDispatch();
 
   function handleSubmit(data) {
     console.tron.log(data);
+    dispatch(updateProfileRequest(data));
   }
 
   return (
