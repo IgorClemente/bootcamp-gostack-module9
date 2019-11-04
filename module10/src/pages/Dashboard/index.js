2886ee9c013bd4aa959eaa4a60a81392582c8034
Igor Clemente <igorelectrofest2011@hotmail.com>
2019-11-04 12:45:19 -0300

    BOOTCAMP_GOSTACK_MODULE10_DASHBOARDSCHEDULES_CREATED_04112019

--- module10/src/pages/Dashboard/index.js
+++ module10/src/pages/Dashboard/index.js
@@ -1,21 +1,99 @@
-import React from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
+
+import {
+  format,
+  addDays,
+  subDays,
+  setHours,
+  setMinutes,
+  setSeconds,
+  isBefore,
+  isEqual,
+  parseISO
+} from 'date-fns';
+
+import pt from 'date-fns/locale/pt';
+import { utcToZonedTime } from 'date-fns-tz';
+
+import api from '~/services/api';
 
 import { MdChevronRight, MdChevronLeft } from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, Time } from './styles';
+
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+  const [schedules, setSchedules] = useState([]);
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  useEffect(() => {
+    async function loadSchedule() {
+      const response = await api.get('schedules', {
+        params: { date }
+      });
+      console.tron.warn(response.data);
+      const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+      const data = range.map(hour => {
+        const checkDate = setSeconds(setMinutes(setHours(date, hour), 0), 0);
+        const compareDate = utcToZonedTime(checkDate, timeZone);
+
+        response.data.forEach(a =>
+          console.tron.log(a.date, compareDate, checkDate)
+        );
+
+        return {
+          time: `${hour}:00h`,
+          past: isBefore(compareDate, new Date()),
+          appointment: response.data.find(a =>
+            isEqual(parseISO(a.date), compareDate)
+          )
+        };
+      });
+
+      setSchedules(data);
+    }
+
+    loadSchedule();
+  }, [date]);
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Container>
       <header>
-        <button type="button">
+        <button type="button" onClick={() => handlePrevDay()}>
           <MdChevronLeft size={36} color="#FFF" />
         </button>
-        <strong>30 de Outubro</strong>
-        <button type="button">
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={() => handleNextDay()}>
           <MdChevronRight size={36} color="#FFF" />
         </button>
       </header>
+
+      <ul>
+        {console.tron.log(schedules)}
+        {schedules.map(time => (
+          <Time key={time.time} past={time.past} available={!time.appointment}>
+            <strong>{time.time}</strong>
+            <span>
+              {time.appointment ? time.appointment.user.name : 'Em aberto'}
+            </span>
+          </Time>
+        ))}
+      </ul>
     </Container>
   );
 }
--- module10/src/pages/Dashboard/styles.js
+++ module10/src/pages/Dashboard/styles.js
@@ -23,4 +23,33 @@ export const Container = styled.div`
       margin: 0 15px;
     }
   }
+
+  ul {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 15px;
+    margin-top: 30px;
+  }
+`;
+
+export const Time = styled.li`
+  background: #fff;
+  padding: 20px;
+  border-radius: 4px;
+
+  opacity: ${props => (props.past ? 0.6 : 1)};
+
+  strong {
+    display: block;
+    font-size: 24px;
+    color: ${props => (props.available ? '#999' : '#7159c1')};
+    font-weight: normal;
+  }
+
+  span {
+    display: block;
+    margin-top: 3px;
+    font-size: 14px;
+    color: ${props => (props.available ? '#999' : '#666')};
+  }
 `;
